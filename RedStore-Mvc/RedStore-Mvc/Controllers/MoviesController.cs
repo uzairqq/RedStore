c9127f2cf1bff90e14bc18c90db88b18e9f9063b
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-09 03:35:17 +0500

    created a movie model and crearted a controller of movie and made a method called random and passed the movie object from controller to the view to show name of movie

---
+++ RedStore-Mvc/RedStore-Mvc/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RedStore_Mvc.Models;
+
+namespace RedStore_Mvc.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movies()
+            {
+                Id = 1,
+                Name = "Avengers"
+            };
+            return View(movie);
+        }
+    }
+}
