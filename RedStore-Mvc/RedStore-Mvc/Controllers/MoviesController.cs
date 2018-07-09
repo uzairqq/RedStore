f470f8808b6fb70263ea280cce2849c9be5b9ba4
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-09 05:10:50 +0500

    used Action Paramters  to see the routes techniques

--- RedStore-Mvc/RedStore-Mvc/Controllers/MoviesController.cs
+++ RedStore-Mvc/RedStore-Mvc/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using RedStore_Mvc.Models;
 
 namespace RedStore_Mvc.Controllers
@@ -17,13 +18,28 @@ namespace RedStore_Mvc.Controllers
                 Id = 1,
                 Name = "Avengers"
             };
-            //return View(movie);
-            //return new ViewResult();
-            //return Content("this is random action method");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
-
+            return View(movie);
         }
+        //if action parameter is this :- int movieId then  route will be:- //movies/edit?12  error in this route like :- //movies/edit/12
+        //if action paramter is this :- int id then route can be this :- //movies/edit/12   erro in this route like :-  //movies/edit?12
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+
+        //it will give this 12 and name parameter 
+        // we can also do like this :- Movies?pageIndex=122&sortBy="laraib" to see the change
+        public ActionResult Index(int? pageIndex,string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 12;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+
+            return Content($"pageIndex={pageIndex} and sortBy={sortBy}");
+        }
+
     }
 }
