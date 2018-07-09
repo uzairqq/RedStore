3ae155787045f05c67baca31ce3ffdf2fe9f4ad5
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-09 06:51:21 +0500

    used attribute based routing

--- RedStore-Mvc/RedStore-Mvc/Controllers/MoviesController.cs
+++ RedStore-Mvc/RedStore-Mvc/Controllers/MoviesController.cs
@@ -20,7 +20,7 @@ namespace RedStore_Mvc.Controllers
             };
             return View(movie);
         }
-
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year,int month)
         {
             return Content(year + "/" + month);
