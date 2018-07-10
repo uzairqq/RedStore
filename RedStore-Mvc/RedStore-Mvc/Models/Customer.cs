5ff531e001efa125afe6bfddd3f8b87b61dc2ae4
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-11 00:59:13 +0500

    Added MembershipType Entity in created relationship between customer and membershiptype and updated to database

--- RedStore-Mvc/RedStore-Mvc/Models/Customer.cs
+++ RedStore-Mvc/RedStore-Mvc/Models/Customer.cs
@@ -9,5 +9,7 @@ namespace RedStore_Mvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; } 
     }
 }
