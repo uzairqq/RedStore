64d39ab469c9fd4481256cb42a10539d9141ecf2
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-11 01:29:12 +0500

    Included Required and String Length DataAnnotations in customer entity and updated database

--- RedStore-Mvc/RedStore-Mvc/Models/Customer.cs
+++ RedStore-Mvc/RedStore-Mvc/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace RedStore_Mvc.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; } 
