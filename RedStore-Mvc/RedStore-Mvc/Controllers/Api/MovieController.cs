9ec2fd84ec7a9839dd95030a1f6390e79ae7cc40
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-16 19:04:30 +0500

    Excercise 6 Created Movie Crud Api with mapping,dto's and httpactionresult

---
+++ RedStore-Mvc/RedStore-Mvc/Controllers/Api/MovieController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using RedStore_Mvc.Dto;
+using RedStore_Mvc.Models;
+
+namespace RedStore_Mvc.Controllers.Api
+{
+    public class MovieController : ApiController
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public MovieController()
+        {
+            _dbContext=new ApplicationDbContext();
+        }
+
+        //    /api/movies/
+        public IEnumerable<MovieDto> Get()
+        {
+            return _dbContext.Movies.ToList().Select(Mapper.Map<Movies, MovieDto>);
+        }
+        // api/movies/id
+        public IHttpActionResult Get(int id)
+        {
+            var moviesInDb = _dbContext.Movies.SingleOrDefault(m => m.Id == id);
+            if (moviesInDb == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movies, MovieDto>(moviesInDb));
+        }
+
+        // api/movies/
+        [HttpPost]
+        public IHttpActionResult Post(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movies = Mapper.Map<MovieDto, Movies>(movieDto);
+            _dbContext.Movies.Add(movies);
+            _dbContext.SaveChanges();
+
+            movieDto.Id = movies.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + movies.Id), movieDto);
+        }
+
+        // api /api/movies/ 
+        [HttpPut]
+        public int Put(MovieDto movieDto, int id)
+        {
+            if(!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadGateway);
+
+            var moviesInDb = _dbContext.Movies.Single(i => i.Id == id);
+            if (moviesInDb == null)
+               throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Mapper.Map(movieDto, moviesInDb);
+            _dbContext.SaveChanges();
+
+            return moviesInDb.Id;
+        }
+
+        // api/Delete
+        [HttpDelete]
+        public int Delete(int id)
+        {
+            var movieInDb = _dbContext.Movies.SingleOrDefault(i => i.Id == id);
+            if(movieInDb==null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _dbContext.Movies.Remove(movieInDb);
+            _dbContext.SaveChanges();
+            return movieInDb.Id;
+        }
+    }
+}
---
+++ RedStore-Mvc/RedStore-Mvc/Dto/MovieDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using RedStore_Mvc.Models;
+
+namespace RedStore_Mvc.Dto
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range(1,20)]
+        public int NumberInStock { get; set; }
+       
+        public byte GenreId { get; set; }
+    }
+}
