170ede0cc1f93ff7360cea1c8c1d717841705669
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-24 21:07:25 +0500

    installed glimpse and used output caching

--- RedStore-Mvc/RedStore-Mvc/Controllers/HomeController.cs
+++ RedStore-Mvc/RedStore-Mvc/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace RedStore_Mvc.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        [OutputCache(Duration = 50,Location = OutputCacheLocation.Server)]
         public ActionResult Index()
         {
             return View();
