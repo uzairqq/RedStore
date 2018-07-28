5c59f5716ec7c19e51bc976249d4d8b6817dbe13
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-28 16:47:52 +0500

    Used Toastr for better notifications

--- RedStore-Mvc/RedStore-Mvc/App_Start/BundleConfig.cs
+++ RedStore-Mvc/RedStore-Mvc/App_Start/BundleConfig.cs
@@ -14,7 +14,8 @@ namespace RedStore_Mvc
                         "~/Scripts/bootstrap.js",
                         "~/Scripts/datatables/jquery.datatables.js",
                         "~/Scripts/datatables/datatables.bootstrap.js",
-                        "~/Scripts/typeahead.bundle.js"
+                        "~/Scripts/typeahead.bundle.js",
+                        "~/Scripts/toastr.js"
                 ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -30,7 +31,8 @@ namespace RedStore_Mvc
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/typeahead.css"));
+                      "~/Content/typeahead.css",
+                      "~/Content/toastr.css"));
         }
     }
 }
